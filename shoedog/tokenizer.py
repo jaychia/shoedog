01a11f80c0890c431dcdcd2c768b1ce63fee8c7a
jaychia <jaychia94@gmail.com>
2019-03-17 15:02:29 -0700

    init tokenizer filters

--- shoedog/tokenizer.py
+++ shoedog/tokenizer.py
@@ -1,13 +1,19 @@
 import re
 from collections import namedtuple
+from shoedog.consts import integer_only_ops, valid_ops, valid_selectors
 
 
 class Toks:
-    OpenObjectToken = namedtuple('OpenObjectToken', ['query_model'])
-    AttributeToken = namedtuple('AttributeToken', ['attribute_name'])
-    FilterToken = namedtuple('FilterToken', ['left', 'op', 'right'])
-    CloseObjectToken = namedtuple('CloseObjectToken', [])
     CastToken = namedtuple('CastToken', ['cast_class'])
+    OpenObjectToken = namedtuple('OpenObjectToken', ['query_model'])
+    CloseObjectToken = namedtuple('CloseObjectToken', [])
+    AttributeToken = namedtuple('AttributeToken', ['attribute_name'])
+    FilterBoolToken = namedtuple('FilterBoolToken', ['sel', 'op', 'val'])
+    FilterBinaryLogicToken = namedtuple('FilterBinaryLogicToken', ['logic_op'])
+    FilterOpenParanToken = namedtuple('FilterOpenParanToken', [])
+    FilterCloseParanToken = namedtuple('FilterCloseParanToken', [])
+    FilterStartToken = namedtuple('FilterStartToken', [])
+    FilterEndToken = namedtuple('FilterEndToken', [])
 
 
 class LineToks:
@@ -17,9 +23,9 @@ class LineToks:
 
 
 regexes = {
-    LineToks.OpenObjectLine: re.compile(r'(?P<query_model>[A-Z]\w*)(?P<cast_class> \([A-Z]\w*\))? {'),
-    LineToks.CloseObjectLine: re.compile(r'\s*}\s*'),
-    LineToks.AttributeLine: re.compile(r"(?P<attribute_name>\w*)(?P<filters>\[[\w|\s|<|>|<=|>=|==|']+\])?$")
+    LineToks.OpenObjectLine: re.compile(r'^(?P<query_model>[A-Z]\w*)(?P<cast_class> \([A-Z]\w*\))? {$'),
+    LineToks.CloseObjectLine: re.compile(r'^}$'),
+    LineToks.AttributeLine: re.compile(r"^(?P<attribute_name>\w*)[\s]?(?P<filters>\[.+\])?$")
 }
 
 
@@ -30,9 +36,60 @@ def _validate_line(i, match, match_tok, line):
                           f' but received `{line}` instead')
 
 
-def _get_filters_from_string(s):
-    print(f'Trying to get filter from {s}')
-    return tuple()
+def _validate_filter(match_obj, filter_string, lexer_ptr):
+    """ Helper to throw errors based on matched filter """
+    if not match_obj:
+        raise SyntaxError(
+            f'Could not parse filter {filter_string}\n{" " * len("SyntaxError: Could not parse filter ")}'
+            f'{" " * lexer_ptr}^ SyntaxError starting here\n'
+        )
+    s = match_obj.group('subject')
+    op = match_obj.group('op')
+    obj = match_obj.group('object')
+    if not s or not op or not obj:
+        raise SyntaxError(f'Unable to parse filter {s} {op} {obj}')
+    if s not in valid_selectors:
+        raise SyntaxError(f'Unable to parse selector {s}')
+    if op not in valid_ops:
+        raise SyntaxError(f'Invalid filter op {op}')
+
+
+filter_regex = re.compile(r"^(?P<subject>\*|all|any) (?P<op>[^\s]+) (?P<object>'?\w+'?)")
+
+
+def _get_filter_tokens_from_string(s):
+    filter_toks = tuple()
+    filters = s[1:-1]
+
+    lexer_ptr = 0
+    while lexer_ptr < len(filters):
+        c = filters[lexer_ptr]
+        if c == ' ':
+            lexer_ptr += 1
+            continue
+        elif c == '(':
+            filter_toks += (Toks.FilterOpenParanToken(),)
+            lexer_ptr += 1
+            continue
+        elif c == ')':
+            filter_toks += (Toks.FilterCloseParanToken(),)
+            lexer_ptr += 1
+            continue
+        elif filters[lexer_ptr:lexer_ptr+4] == 'and ':
+            filter_toks += (Toks.FilterBinaryLogicToken(logic_op='and'),)
+            lexer_ptr += len('and ')
+            continue
+        elif filters[lexer_ptr:lexer_ptr+4] == 'or ':
+            filter_toks += (Toks.FilterBinaryLogicToken(logic_op='or'),)
+            lexer_ptr += len('or ')
+            continue
+        else:
+            print(filter_regex.match(filters[lexer_ptr:]))
+            m = filter_regex.match(filters[lexer_ptr:])
+            _validate_filter(m, s, lexer_ptr)
+            filter_toks += (Toks.FilterBoolToken(sel=m.group('subject'), op=m.group('op'), val=m.group('object')),)
+            lexer_ptr += m.end() - m.start()
+    return filter_toks
 
 
 def tokenize(querystring):
@@ -57,15 +114,18 @@ def tokenize(querystring):
         if isinstance(line_token, LineToks.OpenObjectLine):
             ast_tokens += (Toks.OpenObjectToken(query_model=line_token.query_model),)
             if line_token.cast_class:
-                ast_tokens += (Toks.CastToken(cast_class=line_token.cast_class))
+                ast_tokens += (Toks.CastToken(cast_class=line_token.cast_class),)
         elif isinstance(line_token, LineToks.AttributeLine):
-            ast_tokens += (Toks.AttributeToken(attribute_name=line_token.attribute_name))
+            ast_tokens += (Toks.AttributeToken(attribute_name=line_token.attribute_name),)
             if line_token.filters:
-                ast_tokens += _get_filters_from_string(line_token.filters)
+                ast_tokens += (Toks.FilterStartToken(),)
+                ast_tokens += _get_filter_tokens_from_string(line_token.filters)
+                ast_tokens += (Toks.FilterEndToken(),)
         elif isinstance(line_token, LineToks.CloseObjectLine):
             ast_tokens += (Toks.CloseObjectToken(),)
         else:
             raise NotImplementedError(f'Internal Error: Token parsing for'
                                       f'{match_tok} not implemented')
         tokens += ast_tokens
+
     return tokens
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -2,13 +2,42 @@ import pytest
 from shoedog.tokenizer import Toks, tokenize
 
 
-def test_basic_tokenizer():
+def test_basic_tokenizer_no_fields():
     query = '''
        Sample {
        }
     '''
     assert tokenize(query) == (
         Toks.OpenObjectToken(query_model='Sample'),
+        Toks.CloseObjectToken(),
+    )
+
+
+def test_tokenizer_one_field():
+    query = '''
+       Sample {
+         field1
+       }
+    '''
+    assert tokenize(query) == (
+        Toks.OpenObjectToken(query_model='Sample'),
+        Toks.AttributeToken(attribute_name='field1'),
+        Toks.CloseObjectToken(),
+    )
+
+
+def test_tokenizer_one_field_filtered():
+    query = '''
+       Sample {
+         field1 [* == 'hi']
+       }
+    '''
+    assert tokenize(query) == (
+        Toks.OpenObjectToken(query_model='Sample'),
+        Toks.AttributeToken(attribute_name='field1'),
+        Toks.FilterStartToken(),
+        Toks.FilterBoolToken(sel='*', op='==', val="'hi'"),
+        Toks.FilterEndToken(),
         Toks.CloseObjectToken(),
     )
 
