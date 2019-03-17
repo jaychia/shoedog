6204eb137f9d229c4da75c4d2cdd74715e9821ab
jaychia <jaychia94@gmail.com>
2019-03-16 18:21:14 -0700

    better tokenizing with line token

--- README.md
+++ README.md
@@ -18,16 +18,16 @@ The above code will set up a `/shoedog` endpoint on your application that takes 
 
 ```
 Company {
-	name
+    name
     year_founded [(* > 1994 and * < 2017) or * == 2019]
-  - investors {
-		name [any in ['gv', a16z']]
-		amount_invested [all > 2000000]
-	}
-	contact_details (SingaporeContact) {
-		address
-		singapore_contact_number
-	}
+    investors {
+        name [any in ['gv', a16z']]
+        amount_invested [all > 2000000]
+    }
+    contact_details (SingaporeContact) {
+        address
+        singapore_contact_number
+    }
 }
 ```
 
@@ -40,16 +40,19 @@ Furthermore, the json response has a predictable structure, returning only the f
 
 ```
 {'companies': [{
-	'name': 'XYZ Company',
-	'year_founded': 1995,
-	'contact_details': {
-		'address': '123 Foobar Road',
-		'singapore_contact_number': '+65 9635 9999'
-	}
+    'name': 'XYZ Company',
+    'year_founded': 1995,
+    'investors': [{
+        'name': 'gv',
+        'amount_invested': 2000001 
+    }],
+    'contact_details': {
+        'address': '123 Foobar Road',
+        'singapore_contact_number': '+65 9635 9999'
+    }
 }, ...]}
 ```
 
 Notice that
-1. The investors were not returned, because of the `-` specified in the query it is only used as a filter
-2. The `contact_details` was returned as a dictionary, since it is a different model.
-3. The `contact_details` field was cast as a `SingaporeContact`, which allows us to query for the `singapore_contact_number` field which is only on that particular subclass. This query would have failed without the cast!
+1. The `contact_details` was returned as a dictionary, since it is a different model.
+2. The `contact_details` field was cast as a `SingaporeContact`, which allows us to query for the `singapore_contact_number` field which is only on that particular subclass. This query would have failed without the cast!
--- shoedog/tokenizer.py
+++ shoedog/tokenizer.py
@@ -6,21 +6,33 @@ class Toks:
     OpenObjectToken = namedtuple('OpenObjectToken', ['query_model'])
     AttributeToken = namedtuple('AttributeToken', ['attribute_name'])
     FilterToken = namedtuple('FilterToken', ['left', 'op', 'right'])
-    CloseObjectToken = namedtuple('ClosedObjectToken', [])
+    CloseObjectToken = namedtuple('CloseObjectToken', [])
     CastToken = namedtuple('CastToken', ['cast_class'])
 
 
+class LineToks:
+    OpenObjectLine = namedtuple('OpenObjectLine', ['query_model', 'cast_class'])
+    AttributeLine = namedtuple('AttributeLine', ['attribute_name', 'filters'])
+    CloseObjectLine = namedtuple('CloseObjectLine', [])
+
+
 regexes = {
-    Toks.OpenObjectToken: re.compile(r'(?P<query_model>[A-Z]\w*) {'),
-    Toks.CloseObjectToken: re.compile(r'\s*}\s*'),
+    LineToks.OpenObjectLine: re.compile(r'(?P<query_model>[A-Z]\w*)(?P<cast_class> \([A-Z]\w*\))? {'),
+    LineToks.CloseObjectLine: re.compile(r'\s*}\s*'),
+    LineToks.AttributeLine: re.compile(r"(?P<attribute_name>\w*)(?P<filters>\[[\w|\s|<|>|<=|>=|==|']+\])?$")
 }
 
 
 def _validate_line(i, match, match_tok, line):
     """ Helper to throw errors based on state of tokenizer """
-    if i == 0 and match_tok.__name__ != Toks.OpenObjectToken.__name__:
+    if i == 0 and match_tok.__name__ != LineToks.OpenObjectLine.__name__:
         raise SyntaxError(f'line {i+1}: Expected a query model on the first line'
                           f' but received `{line}` instead')
+
+
+def _get_filters_from_string(s):
+    print(f'Trying to get filter from {s}')
+    return tuple()
 
 
 def tokenize(querystring):
@@ -39,6 +51,21 @@ def tokenize(querystring):
 
         _validate_line(i, match, match_tok, line)
 
-        tokens += (match_tok(**match.groupdict()),)
+        ast_tokens = tuple()
+        line_token = match_tok(**match.groupdict())
 
+        if isinstance(line_token, LineToks.OpenObjectLine):
+            ast_tokens += (Toks.OpenObjectToken(query_model=line_token.query_model),)
+            if line_token.cast_class:
+                ast_tokens += (Toks.CastToken(cast_class=line_token.cast_class))
+        elif isinstance(line_token, LineToks.AttributeLine):
+            ast_tokens += (Toks.AttributeToken(attribute_name=line_token.attribute_name))
+            if line_token.filters:
+                ast_tokens += _get_filters_from_string(line_token.filters)
+        elif isinstance(line_token, LineToks.CloseObjectLine):
+            ast_tokens += (Toks.CloseObjectToken(),)
+        else:
+            raise NotImplementedError(f'Internal Error: Token parsing for'
+                                      f'{match_tok} not implemented')
+        tokens += ast_tokens
     return tokens
