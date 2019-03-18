0e275d55522af97547e787efcc4e78c0626c7db6
jaychia <jaychia94@gmail.com>
2019-03-17 22:51:18 -0700

    only need some in registry

--- shoedog/registry.py
+++ shoedog/registry.py
@@ -1,4 +1,5 @@
 from sqlalchemy import inspect
+from shoedog.errors import ModelNotFoundException
 
 
 class ModelRegistry:
@@ -8,28 +9,37 @@ class ModelRegistry:
         The registry provides convenience functions to work with the app's
         SQLAlchemy models
         """
-        self._tablename_to_models = {
-            model.__tablename__: model for model in models
+        self._name_to_models = {
+            model.__name__: model for model in models
         }
-        self._tablename_to_cols = {
-            model.__tablename__: set(inspect(model).columns.keys())
-            for model in models
-        }
-        self._tablename_to_rels = {
-            model.__tablename__: set(inspect(model).relationships.keys())
-            for model in models
-        }
-        self._tablename_to_poly_child = {
-            model.__tablename__:
-            {polymodel.__tablename__ for polymodel in
-             inspect(model).polymorphic_iterator
-             if polymodel != model} for model in models
-        }
-        self._tablename_to_poly_parent = {
-            child: model_tablename
-            for model_tablename in self._tablename_to_poly_child
-            for child in self._tablename_to_poly_child[model_tablename]
-        }
+        # self._tablename_to_models = {
+        #     model.__tablename__: model for model in models
+        # }
+        # self._tablename_to_cols = {
+        #     model.__tablename__: inspect(model).columns
+        #     for model in models
+        # }
+        # self._tablename_to_rels = {
+        #     model.__tablename__: inspect(model).relationships
+        #     for model in models
+        # }
+        # self._tablename_to_poly_child = {
+        #     model.__tablename__:
+        #     {polymodel.__tablename__ for polymodel in
+        #      inspect(model).polymorphic_iterator
+        #      if polymodel != model} for model in models
+        # }
+        # self._tablename_to_poly_parent = {
+        #     child: model_tablename
+        #     for model_tablename in self._tablename_to_poly_child
+        #     for child in self._tablename_to_poly_child[model_tablename]
+        # }
+
+    def get_model_with_name(self, root_model_name):
+        model = self._name_to_models.get(root_model_name)
+        if not model:
+            raise ModelNotFoundException(f'Could not find model with name {root_model_name}')
+        return model
 
 
 def build_registry(db):
