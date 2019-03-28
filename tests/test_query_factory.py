ecf55968c900bcec194c664a633795b1b740574a
jaychia <jaychia94@gmail.com>
2019-03-28 01:51:47 -0700

    more tests

--- tests/test_query_factory.py
+++ tests/test_query_factory.py
@@ -55,7 +55,6 @@ def test_end_to_end_1(session):
 
     # TODO: Change test after implementing returning only
     # specified fields
-    print(json_response)
     assert json_response == \
         [{'date': '2017-01-02',
           'id': 4,
@@ -189,3 +188,101 @@ def test_end_to_end_2(session):
                    'self_tube_id': None,
                    'type': None},
           'tube_id': 1}]
+
+
+def test_end_to_end_3(session):
+    q = '''
+        query Sample {
+            id
+            tube {
+                name [* == 'only-this-tube']
+                self_tube {
+                    name [* == 'only-this-tube-self-tube']
+                }
+            }
+            self_sample {
+                name [* == 'only-this-self-sample']
+                tubes {
+                    name [all == 'all equal to this']
+                }
+            }
+        }
+    '''
+    # The only sample that fits the criteria
+    sample_1 = Sample(
+        name='I should be the only one being returned',
+        tube=Tube(name='only-this-tube', self_tube=Tube(name='only-this-tube-self-tube')),
+        self_sample=Sample(
+            name='only-this-self-sample',
+            tubes=[
+                Tube(name='all equal to this'),
+                Tube(name='all equal to this'),
+            ])
+    )
+    session.add(sample_1)
+    sample_2 = Sample(
+        tube=Tube(name='bad', self_tube=Tube(name='only-this-tube-self-tube')),
+        self_sample=Sample(
+            name='only-this-self-sample',
+            tube=Tube(self_tube=Tube(name='only-this-self-sample-tube-self-tube')))
+    )
+    session.add(sample_2)
+    sample_3 = Sample(
+        tube=Tube(name='only-this-tube', self_tube=Tube(name='only-this-tube-self-tube')),
+        self_sample=Sample(
+            name='only-this-self-sample',
+            tubes=[
+                Tube(name='all not equal to this'),
+                Tube(name='all equal to this'),
+            ])
+    )
+    session.add(sample_3)
+    sample_4 = Sample(
+        tube=Tube(name='only-this-tube', self_tube=Tube(name='only-this-tube-self-tube')),
+        self_sample=Sample(
+            name='only-this-self-sample',
+            tubes=[])
+    )
+    session.add(sample_4)
+    session.flush()
+
+    json_response = qf.parse_query(q)
+
+    # TODO: Change test after implementing returning only
+    # specified fields
+    assert json_response == \
+        [{'date': None,
+          'id': 1,
+          'name': 'I should be the only one being returned',
+          'self_sample': {'date': None,
+                          'id': 5,
+                          'name': 'only-this-self-sample',
+                          'self_sample_id': 1,
+                          'tube_id': None,
+                          'tubes': [{'date': None,
+                                     'id': 11,
+                                     'name': 'all equal to this',
+                                     'sample_id': 5,
+                                     'self_tube_id': None,
+                                     'type': None},
+                                    {'date': None,
+                                     'id': 12,
+                                     'name': 'all equal to this',
+                                     'sample_id': 5,
+                                     'self_tube_id': None,
+                                     'type': None}]},
+          'self_sample_id': None,
+          'tube': {'date': None,
+                   'id': 1,
+                   'name': 'only-this-tube',
+                   'sample_id': None,
+                   'self_tube': {'date': None,
+                                 'id': 6,
+                                 'name': 'only-this-tube-self-tube',
+                                 'sample_id': None,
+                                 'self_tube_id': 1,
+                                 'type': None},
+                   'self_tube_id': None,
+                   'type': None},
+          'tube_id': 1}]
+
